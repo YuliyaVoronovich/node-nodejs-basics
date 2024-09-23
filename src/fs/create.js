56a7e74ce333a3c958708e5fd29b1c6cf6f07c32
YuliyaVoronovich <Y-1306@mail.ru>
2024-09-23 15:17:39 +0300

    feat: add solution fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { promises } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filesFolder = resolve(__dirname, 'files');
+const fileName = 'fresh.txt';
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await promises.writeFile(join(filesFolder, fileName), fileContent, { flag: 'wx' });
+    console.log(`File ${fileName} created successfully!`);
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      console.log('FS operation failed');
+    } else {
+      console.error(error.message);
+    }
+  }
 };
 
 await create();
